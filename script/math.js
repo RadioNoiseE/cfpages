8196bdc2f319d2796fb5fdb760525c69bd2da2c5
RadioNoiseE <j18516785606@icloud.com>
2025-03-03 15:09:45 +0800

    sync 2025 03 03

--- script/math.js
+++ script/math.js
@@ -1,23 +1,31 @@
 window.MathJax = {
     startup: {
-        elements: ['span.math', 'div.math']
+        elements: ['span.math', 'div.math'],
+    },
+    output: {
+        font: 'mathjax-asana',
     },
     options: {
         enableMenu: false,
+        enableEnrichment: false,
+        enableComplexity: false,
+        enableExplorer: false,
     },
     tex: {
         inlineMath: [['\\(', '\\)']],
         displayMath: [['\\[', '\\]']],
         processEscapes: false,
         processEnvironments: false,
-        processRefs: false
+        processRefs: false,
     },
     svg: {
-        fontCache: 'global'
+        fontCache: 'global',
+        mtextInheritFont: false,
+        merrorInheritFont: false,
     }
 };(function () {
     var script = document.createElement('script');
-    script.src = '/script/tex-svg.js';
+    script.src = '/script/tex-svg-nofont.js';
     script.async = true;
     document.head.appendChild(script);
 })();
