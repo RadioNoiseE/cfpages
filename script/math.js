4baa1a5c7b52fef207442b37b1ce87460dc8a421
RadioNoiseE <j18516785606@icloud.com>
2025-03-28 09:06:20 +0800

    sync 2025 03 28

--- script/math.js
+++ script/math.js
@@ -1,34 +1,26 @@
 window.MathJax = {
-    startup: {
-        elements: ['span.math', 'div.math'],
-    },
-    loader: {
-        load: ['ui/lazy'],
-    },
-    output: {
-        font: 'mathjax-asana',
-    },
+    startup: { elements: ['span.math', 'div.math'] },
+    loader: { load: ['ui/lazy'] },
+    output: { font: 'mathjax-modern' },
     options: {
         enableMenu: false,
-        menuOptions: {
-            settings: {
-                enrich: false,
-            },
-        },
+        menuOptions: { settings: { enrich: false } }
     },
     tex: {
         inlineMath: [['\\(', '\\)']],
         displayMath: [['\\[', '\\]']],
         processEscapes: false,
         processEnvironments: false,
-        processRefs: false,
+        processRefs: false
     },
     svg: {
         fontCache: 'global',
         mtextInheritFont: false,
-        merrorInheritFont: false,
+        merrorInheritFont: false
     }
-};(function () {
+};
+
+(() => {
     var script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/npm/mathjax@4.0.0-beta.7/tex-svg-nofont.js';
     script.async = true;
