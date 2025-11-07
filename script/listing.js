5dfb37dbf82af41d049c6928221aaf6ff4c32b7f
RadioNoiseE <rne.kou@icloud.com>
2025-11-07 22:12:57 +0800

    sync 2025 11 07

--- script/listing.js
+++ script/listing.js
@@ -1,11 +1,11 @@
-import { codeToHtml } from 'https://esm.sh/shiki@3.0.0';
+import { codeToHtml } from 'https://esm.sh/shiki@3.15.0';
 
 const highlight = async listing => {
         const line = Array.from(listing.querySelectorAll('code'));
         const wrap = Array.from(Object.assign(document.createElement('div'), {
                 innerHTML: await codeToHtml(line.map(line => line.textContent).join('\n'), {
                         lang: listing.className.trim(),
-                        theme: 'github-light'
+                        theme: 'gruvbox-light-soft'
                 })
         }).querySelectorAll('.line')).map(span => span.innerHTML);
 
